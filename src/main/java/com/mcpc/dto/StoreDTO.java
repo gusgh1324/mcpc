65ead42f7565c5c57fcdbfbf1a7b666aef83c016
shin-yusoo <yusoo92@naver.com>
2024-02-29 10:33:58 +0900

    storedto 수정

--- src/main/java/com/mcpc/dto/StoreDTO.java
+++ src/main/java/com/mcpc/dto/StoreDTO.java
@@ -1,16 +1,25 @@
 package com.mcpc.dto;
 
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class StoreDTO {
-	private String storeName; // 상호 
-	private String stroreImg; // 가게 이미지+로고
-	private int star; //별점 
-	private String foodType; // 음식종류 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long storeId; // 가게 아이디, 자동으로 부여
 	
-	private String offDay; // 휴무
-	private int operatingHours; // 엽업시간
+	private String storeName; // 상호명
+	private String storePassword; // 가게 비밀번호
 	
-	private String mainDish; // 대표음식  
-	private String mainDishImg; // 대표음식 이미지
-	private String menuImg; // 메뉴판 이미지
-	private int mainDishPrice; // 대표음식 가격
+	private String category; // 카테고리
+	private String image; // 이미지
+	private String mainMenu; // 대표 메뉴
+	private String menuPrice; // 대표 메뉴 가격
+	private String priminum; // 광고
+	private String offDay; // 휴무일
 }
