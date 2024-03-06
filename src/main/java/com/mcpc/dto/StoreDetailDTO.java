b348e98c0decc2bcbdd3b2a4871825a785d1ae31
DESKTOP-1N5612G\it <gusgh1324@naver.com>
2024-03-06 11:14:47 +0900

    수정,ㅡ,,ㅡ,ㅡ

--- src/main/java/com/mcpc/dto/StoreDetailDTO.java
+++ src/main/java/com/mcpc/dto/StoreDetailDTO.java
@@ -14,35 +14,36 @@ import lombok.Setter;
 
 @Getter
 @Setter
-//@Entity
-//@Table(name="storedetail")
+@Entity
+@Table(name="storedetail")
 public class StoreDetailDTO {
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "storeid")
-	private Long storeId; // 가게 아이디
-	
-	private String priminum; // 광고
-	
-	private String category; // 카테고리
-	
-	@Column(name = "offday")
-	private String offDay; // 휴무일
-	
-	// 대표 메뉴
-	@Column(name = "mainimage")
-	private String mainImage; // 이미지
-	@Column(name = "mainmenu")
-	private String mainMenu; // 대표 메뉴
-	@Column(name = "menuprice")
-	private String menuPrice; // 대표 메뉴 가격
-	
-	// 일반 메뉴
-	private String image; // 이미지
-	private String menu; // 메뉴 이름
-	private int price; // 가격
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "storeid")
+    private Long storeId; // 가게 아이디
+    
+    private String priminum; // 광고
+    
+    private String category; // 카테고리
+    
+    @Column(name = "offday")
+    private String offDay; // 휴무일
+    
+    // 대표 메뉴
+    @Column(name = "mainimage")
+    private String mainImage; // 이미지
+    @Column(name = "mainmenu")
+    private String mainMenu; // 대표 메뉴
+    @Column(name = "menuprice")
+    private String menuPrice; // 대표 메뉴 가격
+    
+    // 일반 메뉴
+    private String image; // 이미지
+    private String menu; // 메뉴 이름
+    private int price; // 가격
 
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "storeId")
-	private StoreDTO storeDTO; // StoreDTO와의 일대일 관계
-	
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "storeid")
+    private StoreDTO storeDTO; // StoreDTO와의 일대일 관계
+    
 }
