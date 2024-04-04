fadfe5fe0a3aaad57cf44f0a57c5fcab35b0531c
sko3on <tmdgns0311@naver.com>
2024-04-04 12:30:41 +0900

    8강 > 결승까지 80%구현 - 1대1 매칭 4번 반복 구현 해야함 결승에서 메뉴 선택시 상세정보 넘어가는거 구현 해야함

--- src/main/java/com/mcpc/controller/WorldCupController.java
+++ src/main/java/com/mcpc/controller/WorldCupController.java
@@ -2,6 +2,8 @@ package com.mcpc.controller;
 
 import com.mcpc.dto.WorldCupDTO;
 import com.mcpc.service.WorldCupService;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,21 +15,49 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/worldCup")
+@RequestMapping
 public class WorldCupController {
 
     private final WorldCupService worldCupService;
 
-    @GetMapping
-    public String worldCup(Model model) {
+    @GetMapping("/worldCup")
+    public String worldCup(Model model, HttpSession session) {
         List<WorldCupDTO> menuList = worldCupService.getRandomMenus(); // getRandomMenus 메서드 호출
         model.addAttribute("menuList", menuList);
         List<Integer> test = new ArrayList<Integer>();
         test.add(1);
         test.add(2);
-        
+
         model.addAttribute("test",test);
+        session.setAttribute("selectedMnums", new ArrayList<String>()); // 세션에 선택된 메뉴 번호를 저장할 배열 초기화
         return "worldCup";
     }
 
+    // 선택된 메뉴 번호를 세션에 저장하는 메서드
+    @GetMapping("/saveMnum")
+    public String saveMnum(String mnum, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<String> selectedMnums = (List<String>) session.getAttribute("selectedMnums");
+        selectedMnums.add(mnum);
+        session.setAttribute("selectedMnums", selectedMnums);
+
+        // 만약 선택된 메뉴 번호가 4개라면 SecondMatch 페이지로 이동
+        if (selectedMnums.size() == 4) {
+            return "redirect:/secondMatch";
+        }
+
+        // 아직 4개가 아니면 현재 페이지 유지
+        return "redirect:/worldCup";
+    }
+
+    // SecondMatch 페이지로 이동하는 메서드
+    @GetMapping("/secondMatch")
+    public String secondMatch() {
+        return "secondMatch";
+    }
+    // finalMatch 페이지로 이동하는 메서드
+    @GetMapping("/finalMatch")
+    public String finalMatch() {
+    	return "finalMatch";
+    }
 }
